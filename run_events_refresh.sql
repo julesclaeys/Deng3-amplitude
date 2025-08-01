7f33d01b531dfece2189c40c7eccf59d80f57dd8
Jules Claeys <72757499+julesclaeys@users.noreply.github.com>
2025-08-01 11:34:10 +0100

    Delete run_events_refresh.sql

--- run_events_refresh.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TASK RUN_EVENTS_REFRESH
-warehouse = dataschool_wh
-WHEN SYSTEM$STREAM_HAS_DATA('RAW_EVENTS_STREAM')
-as
-CALL REFRESH_S_AMPLITUDE_EVENTS();
-
--- This code creats a task to run our merge procedure
